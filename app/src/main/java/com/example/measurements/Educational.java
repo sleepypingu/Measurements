08fc3578d01918d1a12a80bd7f9f937e449d8e97
sleepypingu <kardildaryl@gmail.com>
2021-04-27 20:11:22 +0800

    Phenomena_and_illusions

---
+++ app/src/main/java/com/example/measurements/Educational.java
@@ -0,0 +1,14 @@
+package com.example.measurements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Educational extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_educational);
+    }
+}
--- app/src/main/java/com/example/measurements/MainActivity.java
+++ app/src/main/java/com/example/measurements/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.measurements;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.widget.Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (android.widget.Button) findViewById(R.id.button);
+        button.setOnClickListener(new android.view.View.OnClickListener()
+        {
+            @Override
+            public void onClick(android.view.View v) {
+                openEducational();
+            }
+        });
+    }
+
+    public void openEducational(){
+        Intent intent = new Intent(this, Educational.class);
+        startActivity(intent);
     }
 }
