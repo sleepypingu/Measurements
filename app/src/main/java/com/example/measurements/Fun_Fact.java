bb7dd6517511d20d552fc78e3bc01144ce2ddb6b
mofosquito <esjole@protonmail.com>
2021-05-21 11:55:51 +0800

    fun fact

---
+++ app/src/main/java/com/example/measurements/Fun_Fact.java
@@ -0,0 +1,14 @@
+package com.example.measurements;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Fun_Fact extends AppCompatActivity {
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        setContentView(R.layout.activity_fun_fact);
+
+    }
+}
