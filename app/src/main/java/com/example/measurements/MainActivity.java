5340550316a72b64658c069f38bcfcb4badfbfc4
sleepypingu <kardildaryl@gmail.com>
2021-05-21 10:22:35 +0800

    Phenomena_and_illusions

--- app/src/main/java/com/example/measurements/MainActivity.java
+++ app/src/main/java/com/example/measurements/MainActivity.java
@@ -4,30 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private android.widget.Button button;
+    private android.widget.Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button5 = (android.widget.Button) findViewById(R.id.button5);
         button = (android.widget.Button) findViewById(R.id.button);
-        button.setOnClickListener(new android.view.View.OnClickListener()
-        {
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(android.view.View v) {
-                openEducational();
+            public void onClick(View v) {
+                Intent int1 = new Intent(MainActivity.this, Educational.class);
+                startActivity(int1);
             }
         });
-    }
 
-    public void openEducational(){
-        Intent intent = new Intent(this, Educational.class);
-        startActivity(intent);
-    }
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2 = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(int2);
+            }
+        });
 
+    }
 }
---
+++ app/src/main/java/com/example/measurements/phenomena_and_illusions.java
@@ -0,0 +1,14 @@
+package com.example.measurements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class phenomena_and_illusions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phenomena_and_illusions);
+    }
+}
